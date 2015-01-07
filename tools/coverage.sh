49e0623baf3a956ffa6226f5124fb25afa0b94b4
Cameron Morris <zephyrz2712@gmail.com>
2015-01-07 19:55:41 +0000

    ah path

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,1 +1,1 @@
-coveralls --gcov-options 'strayflux/build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' -e 'strayflux/deps/' -e 'strayflux/build/' -e 'strayflux/blocks/strayfluxinteractive/strayflux/logger/easylogging++.h' --verbose | tr ',' '\n' | grep "'name':"
+coveralls --gcov-options 'strayflux/build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' -e 'strayflux/deps/' -e 'strayflux/build/' -e 'strayflux/blocks/strayfluxinteractive/strayflux/logger/easylogging++.h'
