f6b003fbcb8ef31f205b29881c894f460d69fa88
Cameron Morris <zephyrz2712@gmail.com>
2015-01-07 19:35:42 +0000

    Excludes

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,3 +1,4 @@
 coveralls --gcov-options 'build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' \
--e "/strayflux/deps/" \
--e "/strayflux/blocks/strayfluxinteractive/strayflux/logger/easylogging++.h"
+-e 'deps/' \ 
+-e 'build/' \ 
+-e 'blocks/strayfluxinteractive/strayflux/logger/easylogging++.h'
