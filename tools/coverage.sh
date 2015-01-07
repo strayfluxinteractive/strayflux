8d227f8bf862733b8b3d09e8a2d83b4d838dd678
Cameron Morris <zephyrz2712@gmail.com>
2015-01-07 19:39:20 +0000

    Newlines...

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,4 +1,1 @@
-coveralls --gcov-options 'build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' \
--e 'deps/' \ 
--e 'build/' \ 
--e 'blocks/strayfluxinteractive/strayflux/logger/easylogging++.h'
+coveralls --gcov-options 'build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' -e 'deps/' -e 'build/' -e 'blocks/strayfluxinteractive/strayflux/logger/easylogging++.h'
