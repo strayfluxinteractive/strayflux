09c1b56bb0823ca4cb952305c79776fe5cc57315
Cameron Morris <zephyrz2712@gmail.com>
2015-01-08 19:21:21 +0000

    Removed verbose

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,1 +1,1 @@
-coveralls --gcov-options 'build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' -e 'deps/' -e 'build/google' -e 'build/CMakeFiles' -e 'blocks/strayfluxinteractive/strayflux/logger/easylogging++.h' --verbose
+coveralls --gcov-options 'build/strayfluxinteractive/strayflux/CMakeFiles/strayfluxinteractive_strayflux_test_main.dir/' -e 'deps/' -e 'build/google' -e 'build/CMakeFiles' -e 'blocks/strayfluxinteractive/strayflux/logger/easylogging++.h'
